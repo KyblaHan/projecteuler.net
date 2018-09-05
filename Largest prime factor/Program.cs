0d524efa7844ee553d70291754dcc7cb0cac8fc7
KyblaHan <liberiska@gmail.com>
2018-09-05 23:06:07 +0300

    тест возврата

---
+++ Largest prime factor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ 
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+
+ */
+namespace Largest_prime_factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Multiples of 3 and 5/Program.cs
+++ Multiples of 3 and 5/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
 namespace Multiples_of_3_and_5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int summ=0;
+
+            for (int i = 1; i < 1000; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    summ += i;
+            Console.WriteLine(summ);
         }
     }
 }
